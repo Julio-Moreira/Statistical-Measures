2dd699f8ae2f6a22c06e0c8819feda2181f8b236
julio <JulioCavalcanti@proton.me>
2022-08-13 12:32:36 -0300

    starting index code

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Medidas estatisticas</title>
 </head>
 <body>
-    
+    <main id="itens">
+        <section>
+            <em> m<sub>a</sub> = <sup>s</sup>&frasl;<sub>t</sub> </em> <br>
+            <a href="mediana.php"> média aritimetica </a>
+        </section>
+
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+    </main>
 </body>
 </html>
