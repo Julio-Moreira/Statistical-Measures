b088d873b752c21e5402e4657459dc9921fc2e91
julio <JulioCavalcanti@proton.me>
2022-09-09 15:25:01 -0300

    add files to repository

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Statistical Measures</title>
+</head>
+<body>
+    <main id="itens">
+        <section>
+            <em> m<sub>a</sub> = <sup>s</sup>&frasl;<sub>t</sub> </em> <br>
+            <a href="media.php"> mean </a>
+        </section>
+
+        <section>
+            <em> m<sub>e</sub> = ... x ... </em> <br>
+            <a href="mediana.php"> median </a>
+        </section>
+
+        <section>
+            <em> m<sub>o</sub> = f<sub>x</sub> </em> <br>
+            <a href="moda.php"> mode </a>
+        </section>
+
+        <section>
+            <em> f = f<sub>x</sub> </em> <br>
+            <a href="frequencia.php"> frequency </a>
+        </section>
+    </main>
+
+    <footer>
+        <fieldset> <legend> Info </legend>
+                Developed by Julio C. Moreira (2022)
+        </fieldset>
+    </footer>
+</body>
+</html>
